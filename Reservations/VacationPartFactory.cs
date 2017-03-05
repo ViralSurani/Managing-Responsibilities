687b15a4402fb3d57e2bc417864e7edc03414868
Viral Surani <suraniviral@live.com>
2017-03-05 16:54:37 +0530

    Abstract Factory Pattern

---
+++ Reservations/VacationPartFactory.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservations
+{
+    public class VacationPartFactory : IVacationPartFactory
+    {
+        private IHotelSelector hotelSelector;
+        private IHotelService hotelService;
+        private IAirplaneService airplanService;
+
+        public VacationPartFactory(IHotelSelector hotelSelector, IHotelService hotelService, IAirplaneService airplanService)
+        {
+            this.hotelSelector = hotelSelector;
+            this.hotelService = hotelService;
+            this.airplanService = airplanService;
+        }
+
+        public IVacationPart CreateHotelReservation(string town, string hotelName, DateTime arrivalDate, DateTime leaveDate)
+        {
+            HotelInfo hotelInfo = this.hotelSelector.SelectHotel(town, hotelName);
+            return this.hotelService.MakeBooking(hotelInfo, arrivalDate, leaveDate);
+        }
+
+        public IVacationPart CreateFlight(string companyName, string source, string destination, DateTime date)
+        {
+            return this.airplanService.SelectFlight(companyName, source, destination, date);
+        }
+
+        public IVacationPart CreateFerryBooking(string lineName, bool fromMainland, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IVacationPart CreateDailyTrip(string tripName, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IVacationPart CreateMassage(string salon, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
