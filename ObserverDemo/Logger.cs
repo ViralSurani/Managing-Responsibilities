0d46e3be4f8287af4fd834f38b4f3f860ad52f34
Viral Surani <suraniviral@live.com>
2017-03-11 19:52:41 +0530

    NLog in ObserverDemo

--- ObserverDemo/Logger.cs
+++ ObserverDemo/Logger.cs
@@ -3,19 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
 
 namespace ObserverDemo
 {
     public class Logger
     {
+        private static readonly NLog.Logger _logger = LogManager.GetCurrentClassLogger();
+
         public void AfterDoMore(object sender, Tuple<string, string> data)
         {
             Console.WriteLine("Writing down appended {0}.", data.Item2.ToUpper());
+            _logger.Debug("Writing down appended {0}", data.Item2.ToUpper());
         }
 
         public void AfterDoSomethingWith(object sender, string data)
         {
             Console.WriteLine("Writing down {0}.", data.ToUpper());
+            _logger.Info("Writing down {0}.", data.ToUpper());
         }
     }
 }
